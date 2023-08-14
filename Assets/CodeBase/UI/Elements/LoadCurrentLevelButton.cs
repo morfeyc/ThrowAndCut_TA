6d190f10747105a9e9a65aad75752eb18b23c0a3
Morfey <55273349+morfeyc@users.noreply.github.com>
2023-08-15 02:25:09 +0300

    feat: button to load current* level
    * - level number saved in progress

---
+++ Assets/CodeBase/UI/Elements/LoadCurrentLevelButton.cs
@@ -0,0 +1,39 @@
+﻿using CodeBase.Infrastructure.GameStateMachine.Provider;
+using CodeBase.Infrastructure.GameStateMachine.States;
+using CodeBase.Services.Progress;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Zenject;
+
+namespace CodeBase.UI.Elements
+{
+  public class LoadCurrentLevelButton : MonoBehaviour
+  {
+    [SerializeField] private Button Button;
+
+    private IGameStateMachineProvider _stateMachineProvider;
+    private IProgressService _progressService;
+
+    [Inject]
+    public void Construct(IGameStateMachineProvider stateMachineProvider, IProgressService progressService)
+    {
+      _progressService = progressService;
+      _stateMachineProvider = stateMachineProvider;
+    }
+
+    private void Awake() =>
+      Button.onClick.AddListener(ReloadLevel);
+
+    private void OnDestroy() =>
+      Button.onClick.RemoveAllListeners();
+
+    private void ReloadLevel() =>
+      _stateMachineProvider.Value
+        .Enter<LoadLevelState, int>(_progressService.Progress.CurrentLevelId);
+
+    private void Reset() => 
+      Button = GetComponent<Button>() 
+               ?? null;
+  }
+}
