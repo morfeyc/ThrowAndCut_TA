3e2e3eddcae5880e70d64e87d0dea6269ae76344
Morfey <55273349+morfeyc@users.noreply.github.com>
2023-08-15 14:42:30 +0300

    feat: utils for vector

---
+++ Assets/CodeBase/Utils/VectorUtils.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace CodeBase.Utils
+{
+  public static class VectorUtils
+  {
+    public static Vector3 GetMiddlePoint(Vector3 vectorA, Vector3 vectorB) => 
+      (vectorA + vectorB) / 2;
+  }
+}
