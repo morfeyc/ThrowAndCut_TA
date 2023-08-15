5c8055563c84ff0bbd2af514afa5c8d1c01fc84c
Morfey <55273349+morfeyc@users.noreply.github.com>
2023-08-15 17:05:15 +0300

    feat: add ability to slice objects

---
+++ Assets/CodeBase/Logic/ObjectSlicer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace CodeBase.Logic
+{
+  public class ObjectSlicer : MonoBehaviour
+  {
+    [SerializeField] private LayerMask _sliceableLayer;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+      if (!IsProperLayer(other.gameObject.layer)) 
+        return;
+      if (!other.gameObject.TryGetComponent(out SliceableObject sliceableObject)) 
+        return;
+      
+      sliceableObject.Slice(transform.position, transform.up);
+    }
+
+    private bool IsProperLayer(int layer)
+    {
+      return (_sliceableLayer.value & (1 << layer)) != 0;
+    }
+  }
+}
---
+++ Assets/CodeBase/Logic/SliceableObject.cs
@@ -0,0 +1,32 @@
+﻿using EzySlice;
+using UnityEngine;
+
+namespace CodeBase.Logic
+{
+  public class SliceableObject : MonoBehaviour
+  {
+    [SerializeField] private Material _crossSectionMaterial;
+    
+    public void Slice(Vector3 position, Vector3 direction)
+    {
+      SlicedHull hull = gameObject.Slice(transform.position, transform.up);
+
+      if (hull != null)
+      {
+        GameObject upperHull = hull.CreateUpperHull(gameObject, _crossSectionMaterial);
+        SetupSlicedObject(upperHull);
+        GameObject lowerHull = hull.CreateLowerHull(gameObject, _crossSectionMaterial);
+        SetupSlicedObject(lowerHull);
+
+        Destroy(gameObject);
+      }
+    }
+
+    private void SetupSlicedObject(GameObject slice)
+    {
+      Rigidbody rigidbody = slice.AddComponent<Rigidbody>();
+      MeshCollider meshCollider = slice.AddComponent<MeshCollider>();
+      meshCollider.convex = true;
+    }
+  }
+}
